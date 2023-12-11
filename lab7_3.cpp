296d98810d649aa90bf5d720d5e7b25bb4164b99
Natwaa <Natwara_ch@cmu.ac.th>
2023-12-12 04:24:54 +0700

    7.3 completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -3,6 +3,13 @@ using namespace std;
 
 char before(char x){
 	//Write your function definition here
+	if(int(x)<65 || int(x)>90){
+		return '0';
+	}else if(int(x)==65){
+		return 'Z';
+	}else{
+		return x-1;
+	}
 }
 
 int main(){
