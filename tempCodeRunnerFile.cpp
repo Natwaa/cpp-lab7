cd200a068830854935fe6b64302cd1bfe9d02da5
Natwaa <Natwara_ch@cmu.ac.th>
2023-12-12 04:00:34 +0700

    7.2 completed

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -25,21 +25,21 @@ int main(){
     string name, movie,day;
     cout << "Fahsai: Sawadee ka...Can you tell me your name?"<<endl;
     cout << "?????: ";
-    getline(cin,name);
-    cout <<"Fahsai: Wow!!! "<<name<<" is a really cool name."<<endl;
+    cin >> name;
+    cout << "Fahsai: Wow!!! "<<name<<" is a really cool name."<<endl;
     cout << "Fahsai: I think you are an Engineering student. What is your student ID?"<<endl;
     cout << name << ": ";
     cin >> id;
-    cin.ignore();
-    gear =(id / 10000000)-12;
+    gear =(id / 10000000) -12;
     cout << "Fahsai: I think you may be GEAR "<< gear<<". I have a free movie ticket for you."<<endl;
     cout <<"Fahsai: Let's go to the cinema together!!!"<<endl;
     cout <<"Fahsai: What movie do you want to watch?"<<endl;
     cout << name << ": ";
-    getline(cin,movie) ;
+    cin >> movie ;
 
     cout << "Fahsai: So....which day are you free to go with me?"<<endl;
-    getline(cin,day);
-    cout <<name<< ": Fahsai: "<<day<<"....that is OK!!! I'm looking forward to watching "<<movie<<" with you."<<endl;
-    cout <<name<< ": Fahsai: 555+ see you "<< day <<". Bye Bye \\(^ ^)/";
+    cin >> day;
+    cout << "Fahsai: "<<day<<"....that is OK!!! I'm looking forward to watching movie with you."<<endl;
+    cout << name<<": May the Force be with you krub"<<endl;
+    cout << "Fahsai: 555+ see you "<< day <<". Bye Bye \(^ ^)/";
 }
